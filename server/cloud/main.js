fa1943c829ebc24a8bbdb379196edfcbef9c084b
Luis meza <luimi@live.com>
2024-05-15 15:53:54 -0500

    buyfreecoupon

--- server/cloud/main.js
+++ server/cloud/main.js
@@ -31,3 +31,27 @@ Parse.Cloud.define("generateLink", async (request) => {
     await savedReceipt.save(null, {useMasterKey: true})
     return link
 });
+
+Parse.Cloud.define("buyFreeCoupon", async (request) => {
+    if(!request.user) return {success: false, code: 1}
+    if(!request.params.coupon) return {success: false, code: 2}
+    const coupon = await new Parse.Query("Coupon").include("company").get(request.params.coupon)
+    if(coupon.get("price") !== 0) return {success: false, code: 3}
+    const Receipt = Parse.Object.extend("Receipt");
+    const receipt = new Receipt();
+    const companyRole = await new Parse.Query(Parse.Role).equalTo("name", coupon.get("company").get("name")).first()
+    receipt.set("user", request.user);
+    receipt.set("company", coupon.get("company"));
+    receipt.set("coupon", coupon);
+    receipt.set("active", true);
+    receipt.set("available", true)
+    const acl = new Parse.ACL()
+    acl.setReadAccess(request.user.id, true)
+    acl.setRoleReadAccess(companyRole.id, true)
+    acl.setRoleWriteAccess(companyRole.id, true)
+    receipt.setACL(acl)
+    const savedReceipt = await receipt.save()
+    if(coupon.get("haveAmount")) coupon.set("left", coupon.get("left")-1);
+    coupon.save(null, {useMasterKey: true});
+    return savedReceipt.id;
+})
